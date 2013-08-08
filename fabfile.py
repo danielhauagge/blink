be73a442ef45a11464b61217b24837f658ff98dd
Kevin James Matzen <kmatzen@cs.cornell.edu>
2013-08-08 22:34:56 +0000

    Increased number of workers per instance.

--- fabfile.py
+++ fabfile.py
@@ -40,14 +40,17 @@ env.user = 'ubuntu'
 env.hosts = [i.public_dns_name for i in instances]
 env.key_filename = '/home/kmatzen/kmatzenvision.pem'
 
+@task
 def hostname():
     run('hostname')
 
+@task
 def upgrade():
     install()
     stop()
     start()
 
+@task
 def install():
     try:
         run('ls blink')
@@ -55,38 +58,45 @@ def install():
         run('git clone https://github.com/kmatzen/blink.git')
     configure()
 
+@task
 def configure():
     put('blink.cfg', 'blink/blink.cfg')
 
+@task
 def stop():
     run('tmux kill-session -t blink', warn_only=True)
 
+@task
 def start():
     with cd('blink'):
         run('tmux new-session -d -s blink ./manager.py', pty=False)
 
+@task
 def status():
     run('ps aux|grep python')
 
+@task
+@hosts('localhost')
 def add_instance():
-    env.hosts = ['localhost']
     conn = connect()
     reservation = conn.request_spot_instances(0.06, 'ami-42a6db2b', key_name='kmatzenvision', instance_type='m3.xlarge', availability_zone_group='us-east-1d', security_groups=['kmatzen'])
-    
+   
+    spot_ids = [s.id for s in reservation]
+ 
     while True:
         ready = True
    
-        for r in conn.get_all_spot_instance_requests([s.id for s in reservation]):
+        for r in conn.get_all_spot_instance_requests(spot_ids):
             print('Code: %s'%r.status.code)
             print('Update time: %s'%r.status.update_time)
             print('Message: %s'%r.status.message)
             if r.status.code != 'fulfilled':
                 ready = False
 
-        time.sleep(1)
-
-        if ready == True:
+        if ready:
             break
+
+        time.sleep(1)
 
     existing = set()
 
@@ -97,7 +107,24 @@ def add_instance():
         num = int(groups[0])
         existing.add(num)
 
-    for r in conn.get_all_spot_instance_requests([s.id for s in reservation]):
+    while True:
+        ready = True
+
+        for r in conn.get_all_spot_instance_requests(spot_ids):
+            instance_id = r.instance_id
+            reservations = conn.get_all_instances(instance_id)
+            for reservation in reservations:
+                for instance in reservation.instances:
+                    print('State: %s'%instance.state)
+                    if instance.state != 'running':
+                        ready = False
+
+        if ready:
+            break
+
+        time.sleep(1)
+
+    for r in conn.get_all_spot_instance_requests(spot_ids):
         instance_id = r.instance_id
         reservations = conn.get_all_instances(instance_id)
         for reservation in reservations:
@@ -108,6 +135,5 @@ def add_instance():
                         existing.add(num)
                         break
 
-                if instance.state == 'running':
-                     instance.add_tag('Name', new_name)
+                instance.add_tag('Name', new_name)
                 print('New node named %s'%new_name)
